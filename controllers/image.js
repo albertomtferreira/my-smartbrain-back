0e72054ab9a77b1ceb47a86541a14bbdef6a1b5f
Alberto Ferreira <60177437+albertomtferreira@users.noreply.github.com>
2023-11-15 22:58:17 +0000

    update new version of clarifai API

--- controllers/image.js
+++ controllers/image.js
@@ -1,49 +1,63 @@
-    const handleApiCall = (req,res)=>{
-      // Your PAT (Personal Access Token) can be found in the portal under Authentification
-      const PAT = '5d64e4b1500c49f68d6173563b6facb9';
-      // Specify the correct user_id/app_id pairings
-      // Since you're making inferences outside your app's scope
-      const USER_ID = 'clarifai';       
-      const APP_ID = 'main';
-      // Change these to whatever model and image URL you want to use
-      //Models - https://clarifai.com/explore/models
-      //age-demographics-recognition -- https://clarifai.com/clarifai/main/models/age-demographics-recognition
-      //general-image-recognition -- https://clarifai.com/clarifai/main/models/general-image-recognition
-      //color-recognition -- https://clarifai.com/clarifai/main/models/color-recognition
-      //general-image-detection -- https://clarifai.com/clarifai/main/models/general-image-detection
-      //face-detection -- https://clarifai.com/clarifai/main/models/face-detection
-      const MODEL_ID = 'face-detection';
-      const IMAGE_URL = req.body.input;
-      const raw = JSON.stringify({
-          "user_app_id": {
-              "user_id": USER_ID,
-              "app_id": APP_ID
-          },
-          "inputs": [
-              {
-                  "data": {
-                      "image": {
-                          "url": IMAGE_URL
-                      }
-                  }
-              }
-          ]
-      });
-      const requestOptions = {
-          method: 'POST',
-          headers: {
-              'Accept': 'application/json',
-              'Authorization': 'Key ' + PAT
-          },
-          body: raw
-      };
-      fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
-      .then(data => data.json())
-      .then(data=>{
-        res.json(data)
-      })
-      .catch(err => res.status(400).json('Unable to resolve API'))
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+
+const handleApiCall = (req,res)=>{
+  // Your PAT (Personal Access Token) can be found in the portal under Authentification
+  const PAT = '5d64e4b1500c49f68d6173563b6facb9';
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = 'clarifai';
+  const APP_ID = 'main';
+  // Change these to whatever model and image URL you want to use
+  //Models - https://clarifai.com/explore/models
+  //age-demographics-recognition -- https://clarifai.com/clarifai/main/models/age-demographics-recognition
+  //general-image-recognition -- https://clarifai.com/clarifai/main/models/general-image-recognition
+  //color-recognition -- https://clarifai.com/clarifai/main/models/color-recognition
+  //general-image-detection -- https://clarifai.com/clarifai/main/models/general-image-detection
+  //face-detection -- https://clarifai.com/clarifai/main/models/face-detection
+  const MODEL_ID = 'face-detection';
+  const MODEL_VERSION_ID = '';
+  const IMAGE_URL = req.body.input;
+  ///////////////////////////////////////////////////////////////////////////////////
+  // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+  ///////////////////////////////////////////////////////////////////////////////////
+  const stub = ClarifaiStub.grpc();
+  // This will be used by every Clarifai endpoint call
+  const metadata = new grpc.Metadata();
+  metadata.set("authorization", "Key " + PAT);
+  stub.PostModelOutputs(
+    {
+        user_app_id: {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        model_id: MODEL_ID,
+        version_id: MODEL_VERSION_ID, // This is optional. Defaults to the latest model version
+        inputs: [
+            { data: { image: { url: IMAGE_URL, allow_duplicate_url: true } } }
+        ]
+    },
+    metadata,
+    (err, response) => {
+        if (err) {
+            throw new Error(err);
+        }
+
+        if (response.status.code !== 10000) {
+            throw new Error("Post model outputs failed, status: " + response.status.description);
+        }
+
+        // Since we have one input, one output will exist here
+        const output = response.outputs[0];
+
+        // console.log("Predicted concepts:");
+        // for (const concept of output.data.concepts) {
+        //     console.log(concept.name + " " + concept.value);
+        // }
+        res.json(response)
+        // .catch(err => res.status(400).json('Unable to resolve API'))
     }
+  )
+}
     
 const handleImage = (req,res,db)=>{
   const {id} = req.body;
