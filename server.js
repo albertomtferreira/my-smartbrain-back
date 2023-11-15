9f5d8fe2fbf0de4d26baac5b9367e0dbb47737a9
Alberto Ferreira <60177437+albertomtferreira@users.noreply.github.com>
2023-11-15 20:17:26 +0000

    create register controller

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const saltRounds = 10;
 const port = 3001;
 const knex = require('knex');
+const register = require('./controllers/register');
 
 const db = knex({
   client: 'pg',
@@ -49,33 +50,7 @@ app.post('/signin', (req,res)=>{
 })
 
 //REGISTER
-app.post('/register', (req,res) =>{
-  const {email,name,password}=req.body;
-  const hash = bcrypt.hashSync(password, saltRounds);
-  db.transaction(trx =>{
-    trx.insert({
-      hash: hash,
-      email: email
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail =>{
-      return trx('users')
-        .returning('*')
-        .insert({
-          email: loginEmail[0].email,
-          name: name,
-          joined: new Date()
-        })
-      .then(user =>{
-        res.json(user[0]);
-      })
-    })
-    .then(trx.commit)
-    .catch(trx.rollback)
-  })
-    .catch(err => res.status(400).json('Unable to register!'))
-})
+app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt,saltRounds)})
 
 app.get('/profile/:id',(req,res)=>{
   const {id} = req.params;
