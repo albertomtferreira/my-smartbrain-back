d4e92d5bedf561e884bcc4cdb64f91f23f8733ad
Alberto Ferreira <60177437+albertomtferreira@users.noreply.github.com>
2023-11-16 20:59:07 +0000

    debugging

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
-// const saltRounds = 10;
-// const knex = require('knex');
-// const register = require('./controllers/register');
-// const signin = require('./controllers/signin');
-// const profile = require('./controllers/profile');
-// const image = require('./controllers/image');
+const saltRounds = 10;
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
-// const db = knex({
-//   client: 'pg',
-//   connection: {
-//     host : '127.0.0.1',
-//     port : 5432,
-//     user : 'postgres',
-//     password : '290811',
-//     database : 'smart-brain'
-//   }
-// });
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port : 5432,
+    user : 'postgres',
+    password : '290811',
+    database : 'smart-brain'
+  }
+});
 
 const app = express();
 app.use(express.urlencoded({extended: false}));
@@ -26,19 +26,19 @@ app.use(cors());
 
 //ROOT
 app.get('/', (req,res)=>{res.send('WORKSSSS!!!')})
-// //SIGNIN
-// app.post('/signin', (req,res) => {signin.handleSignin(req,res,db,bcrypt)})
-// //REGISTER
-// app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt,saltRounds)})
-// //PROFILE
-// app.get('/profile/:id', (req,res) => {profile.handleProfileGet(req,res,db)})
-// //IMAGE
-// app.put('/image', (req,res) => {image.handleImage(req,res,db)})
-// //IMAGE-API
-// app.post('/imageurl', (req,res) => {image.handleApiCall(req,res)})
+//SIGNIN
+app.post('/signin', (req,res) => {signin.handleSignin(req,res,db,bcrypt)})
+//REGISTER
+app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt,saltRounds)})
+//PROFILE
+app.get('/profile/:id', (req,res) => {profile.handleProfileGet(req,res,db)})
+//IMAGE
+app.put('/image', (req,res) => {image.handleImage(req,res,db)})
+//IMAGE-API
+app.post('/imageurl', (req,res) => {image.handleApiCall(req,res)})
 
-// app.listen(process.env.PORT || 3500, ()=> {
-//   console.log(`app is running on  port ${process.env.PORT}`);
-// })
+app.listen(process.env.PORT || 3500, ()=> {
+  console.log(`app is running on  port ${process.env.PORT}`);
+})
 
-app.listen(process.env.PORT);
+// app.listen(process.env.PORT);
