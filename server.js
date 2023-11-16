256ecdb3d80a35b75d23f28d72900989dcaee5f7
Alberto Ferreira <60177437+albertomtferreira@users.noreply.github.com>
2023-11-16 22:16:12 +0000

    connect database

--- server.js
+++ server.js
@@ -15,12 +15,14 @@ const { Client } = require('pg');
 //     rejectUnauthorized: false
 //   }
 // });
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true
+    ssl: {
+      rejectUnauthorized: false
+    },
   }
 });
 
